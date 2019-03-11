347f80ffa0741bd98f67abd5668433a2f0a27f6d
germany900 <ninganegro@hotmail.com>
2019-03-11 15:45:25 -0600

    Se implementa el metodo GET

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/hola/:name', (req, res) => {
+	res.send({message: `Hola ${req.params.name}!`});
+});
+
 
 app.listen(port, () => {
 	console.log(`Api Rest corriendo en el localhost:${port}`);
