2d331a8bc6d965ba8e45b9054dbb026fce45affc
germany900 <ninganegro@hotmail.com>
2019-03-12 11:07:04 -0600

    Se implemento busqueda por id funcion 100%

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/api/product', (req, res) => {
-	res.status(200).send({products: []})
+	Product.find({}, (err, products) => {
+		if(err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
+			if(!products) return res.status(404).send({message: 'No existen productos'})
+				res.status(200).send({ products })
+	});
 });
 
-app.get('/api/product/:productId', (req, res) =>{
+app.get('/api/product/:productId', (req, res) => {
+	let productId = req.params.productId;
 
+	Product.findById(productId, (err, product) => {
+		if(err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
+			if(!product) return res.status(404).send({message: `El producto no existe`})
+				res.status(200).send({ product })
+	});
 });
 
 app.post('/api/product/registro', (req, res) => {
